9ae800ba50d1ed7dc7f86a71afbad149f741f0e4
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-05-16 16:41:13 -0400

    navigation to tree detail page view controller

---
+++ TreeSapIOS/BenefitsDisplayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NutritionFactsViewController.swift
+//  TreeSapIOS
+//
+//  Created by Summer2019 on 5/16/19.
+//  Copyright © 2019 Hope CS. All rights reserved.
+//
+
+import UIKit
+
+class BenefitsDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TreeSapIOS/TreeDetailPageViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TreeDetailPageViewController.swift
+//  TreeSapIOS
+//
+//  Created by Summer2019 on 5/16/19.
+//  Copyright © 2019 Hope CS. All rights reserved.
+//
+
+import UIKit
+
+class TreeDetailPageViewController: UIPageViewController
+{
+    fileprivate lazy var pages: [UIViewController] = {
+        return [
+            self.getViewController(withIdentifier: "simpleDisplay"),
+            self.getViewController(withIdentifier: "pieChartDisplay"),
+            self.getViewController(withIdentifier: "benefitsDisplay")
+        ]
+    }()
+    
+    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate   = self
+        
+        if let firstVC = pages.first
+        {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+}
+
+extension TreeDetailPageViewController: UIPageViewControllerDataSource
+{
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0 else { return pages.last }
+        guard pages.count > previousIndex else { return nil }
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex < pages.count else { return pages.first }
+        guard pages.count > nextIndex else { return nil }
+        return pages[nextIndex]
+    }
+}
+
+extension TreeDetailPageViewController: UIPageViewControllerDelegate { }
