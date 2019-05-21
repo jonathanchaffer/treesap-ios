ed11e90a6da55e1deb2ae553034287673662455e
jonathanchaffer <jonathanchaffer@gmail.com>
2019-05-21 14:40:05 -0400

    get tree data by coordinates working

--- TreeSapIOS/controller/CoordinatesViewController.swift
+++ TreeSapIOS/controller/CoordinatesViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class CoordinatesViewController: UIViewController {
+	// MARK: Properties
     @IBOutlet weak var latitudeTextField: UITextField!
     @IBOutlet weak var longitudeTextField: UITextField!
+	let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +21,41 @@ class CoordinatesViewController: UIViewController {
         latitudeTextField.delegate = self
         longitudeTextField.delegate = self
     }
+	
+	// MARK: Actions
+	@IBAction func handleCoordinatesButtonPressed(_ sender: UIButton) {
+		// Convert the inputs to Double. If the conversion failed, alert the user.
+		let latitude = Double(latitudeTextField.text!)
+		let longitude = Double(longitudeTextField.text!)
+		if (latitude != nil && longitude != nil) {
+			// If tree data was found, display it. Otherwise, alert the user.
+			let treeToDisplay = self.getTreeDataByCoords(latitude: latitude!, longitude: longitude!)
+			if (treeToDisplay != nil) {
+				let pages = TreeDetailPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+				pages.displayedTree = treeToDisplay
+				navigationController?.pushViewController(pages, animated: true)
+			} else {
+				let alert = UIAlertController(title: "No trees found", message: "There were no trees found near that location.", preferredStyle: .alert)
+				alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+				self.present(alert, animated: true)
+			}
+		} else {
+			let alert = UIAlertController(title: "Invalid coordinates", message: "Please make sure that you input valid coordinates.", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+			self.present(alert, animated: true)
+		}
+	}
+	
+	// MARK: Private methods
+	private func getTreeDataByCoords(latitude: Double, longitude: Double) -> Tree? {
+		let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+		let dataSets = appDelegate.getDataSets()
+		return TreeFinder.findTree(location: location, dataSets: dataSets)
+	}
+	
 }
 
+// Extension that makes the text fields allow only numbers, dashes, and dots.
 // https://stackoverflow.com/questions/30973044/how-to-restrict-uitextfield-to-take-only-numbers-in-swift
 extension CoordinatesViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
