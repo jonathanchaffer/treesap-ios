3b462293abb24f6e994e89f7b650e2848b5bcf1f
jonathanchaffer <jonathanchaffer@gmail.com>
2019-05-22 10:58:10 -0400

    installed CSVImporter to TreeSapIOSTests

--- Podfile
+++ Podfile
@@ -2,3 +2,6 @@ platform :ios, '12.3'
 target "TreeSapIOS" do
   pod 'CSVImporter'
 end 
+target "TreeSapIOSTests" do
+	pod 'CSVImporter'
+end
---
+++ Pods/Target Support Files/Pods-TreeSapIOSTests/Pods-TreeSapIOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TreeSapIOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_TreeSapIOSTests
+@end
