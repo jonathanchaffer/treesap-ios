44dd534bfde640bf2b26b2f2509dad0f8bfc8c67
Josiah Brett <joisahqbrett@gmail.com>
2019-05-22 16:56:43 -0400

    Added a test class that tests the class NameFormatter.

--- TreeSapIOSTests/NameFormatterTests.swift
+++ TreeSapIOSTests/NameFormatterTests.swift
@@ -7,27 +7,29 @@
 //
 
 import XCTest
+@testable import TreeSapIOS
+import CSVImporter
 
 class NameFormatterTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        func testFormatCommonName(){
+            for index in 0..<commonNameTestStrings.count {
+                XCTAssertEqual(NameFormatter.formatCommonName(commonName: commonNameTestStrings[index]), commonNameExpectedResults[index])
+            }
         }
-    }
-
+        
+        func testFormatScientificName(){
+            for index in 0..<scientificNameTestStrings.count{
+                XCTAssertEqual(NameFormatter.formatScientificName(scientificName: scientificNameTestStrings[index]), scientificNameExpectedResults[index])
+            }
+        }
+    
+        //MARK: Test Cases and Expected Results
+        //common name test cases
+        let commonNameTestStrings: [String] = ["Sugar maple", "Crimson King Maple", "Ginkgo", "Unknown", "Mulberry-Red", "Pine-Eastern White", "Dogwood-Flowering", "Crabapple-Japanese Flowering"]
+        let commonNameExpectedResults: [String] = ["Sugar maple", "Crimson King Maple", "Ginkgo", "Unknown", "Red Mulberry", "Eastern White Pine", "Flowering Dogwood", "Japanese Flowering Crabapple"]
+    
+        //scientific name test cases
+        let scientificNameTestStrings: [String] = ["Acer saccharum", "", "Acer x freemanii", "Acer rubrum", "MORU Morus rubra", "PIAB Picea ables", "MAFL80 Malus floribunda", "ACSA1 Acer saccharinum", "ACSA2 Acer saccharum", "GLTR Geditsia triacanthos"]
+        let scientificNameExpectedResults: [String] = ["Acer saccharum", "", "Acer x freemanii", "Acer rubrum", "Morus rubra", "Picea ables", "Malus floribunda", "Acer saccharinum", "Acer saccharum", "Geditsia triacanthos"]
 }
