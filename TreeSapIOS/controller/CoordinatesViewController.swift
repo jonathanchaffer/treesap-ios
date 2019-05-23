9a436ef23a5ceca796f2b56a25e2413ded368792
Josiah Brett <joisahqbrett@gmail.com>
2019-05-23 13:37:47 -0400

    Added code for the coordinates screen so that pressing the return button while editing the first text field causes the second text field to become selected and pressing enter while editing the second text causes the coordinates to be submitted.

--- TreeSapIOS/controller/CoordinatesViewController.swift
+++ TreeSapIOS/controller/CoordinatesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class CoordinatesViewController: UIViewController {
+class CoordinatesViewController: UIViewController, UITextFieldDelegate {
 	// MARK: Properties
     @IBOutlet weak var latitudeTextField: UITextField!
     @IBOutlet weak var longitudeTextField: UITextField!
@@ -27,50 +27,70 @@ class CoordinatesViewController: UIViewController {
         gestureRecognizer.cancelsTouchesInView = false
         self.view.addGestureRecognizer(gestureRecognizer)
     }
+    
+    func handleCoordinates(){
+        // Convert the inputs to Double. If the conversion failed, alert the user.
+        let latitude = Double(latitudeTextField.text!)
+        let longitude = Double(longitudeTextField.text!)
+        if (latitude != nil && longitude != nil) {
+            // If tree data was found, display it. Otherwise, alert the user.
+            let treeToDisplay = self.getTreeDataByCoords(latitude: latitude!, longitude: longitude!)
+            if (treeToDisplay != nil) {
+                let pages = TreeDetailPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+                pages.displayedTree = treeToDisplay
+                navigationController?.pushViewController(pages, animated: true)
+            } else {
+                let alert = UIAlertController(title: "No trees found", message: "There were no trees found near that location.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: true)
+            }
+        } else {
+            let alert = UIAlertController(title: "Invalid coordinates", message: "Please make sure that you input valid coordinates.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
 	
 	// MARK: Actions
 	@IBAction func handleCoordinatesButtonPressed(_ sender: UIButton) {
-		// Convert the inputs to Double. If the conversion failed, alert the user.
-		let latitude = Double(latitudeTextField.text!)
-		let longitude = Double(longitudeTextField.text!)
-		if (latitude != nil && longitude != nil) {
-			// If tree data was found, display it. Otherwise, alert the user.
-			let treeToDisplay = self.getTreeDataByCoords(latitude: latitude!, longitude: longitude!)
-			if (treeToDisplay != nil) {
-				let pages = TreeDetailPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-				pages.displayedTree = treeToDisplay
-				navigationController?.pushViewController(pages, animated: true)
-			} else {
-				let alert = UIAlertController(title: "No trees found", message: "There were no trees found near that location.", preferredStyle: .alert)
-				alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-				self.present(alert, animated: true)
-			}
-		} else {
-			let alert = UIAlertController(title: "Invalid coordinates", message: "Please make sure that you input valid coordinates.", preferredStyle: .alert)
-			alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-			self.present(alert, animated: true)
-		}
+		handleCoordinates()
 	}
 	
-	// MARK: Private methods
-	private func getTreeDataByCoords(latitude: Double, longitude: Double) -> Tree? {
-		let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-		let dataSets = appDelegate.getDataSets()
-		return TreeFinder.findTree(location: location, dataSets: dataSets)
-	}
-    
+    //MARK: Text-Field-Related Functions
     //Based on code from https://medium.com/@KaushElsewhere/how-to-dismiss-keyboard-in-a-view-controller-of-ios-3b1bfe973ad1
     ///Makes all included text fields (which will probably be all the text fields that this class has as properties) stop editting, dismissing the keyboard
     @objc func stopEditingText(){
         longitudeTextField.endEditing(true)
         latitudeTextField.endEditing(true)
     }
-	
+    
+    /**
+     
+    */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField{
+        case latitudeTextField:
+            longitudeTextField.becomeFirstResponder()
+        case longitudeTextField:
+            handleCoordinates()
+        default:
+            return true
+        }
+        
+        return true
+    }
+    
+	// MARK: Private methods
+	private func getTreeDataByCoords(latitude: Double, longitude: Double) -> Tree? {
+		let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+		let dataSets = appDelegate.getDataSets()
+		return TreeFinder.findTree(location: location, dataSets: dataSets)
+	}
 }
 
 // Extension that makes the text fields allow only numbers, dashes, and dots.
 // https://stackoverflow.com/questions/30973044/how-to-restrict-uitextfield-to-take-only-numbers-in-swift
-extension CoordinatesViewController: UITextFieldDelegate {
+extension CoordinatesViewController {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let allowedCharacters = CharacterSet(charactersIn:"-.0123456789")
         let characterSet = CharacterSet(charactersIn: string)
