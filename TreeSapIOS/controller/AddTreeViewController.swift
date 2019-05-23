2054305ca1ee51c815ba5538edf000012d06fe15
Josiah Brett <joisahqbrett@gmail.com>
2019-05-23 16:37:28 -0400

    Added documentation to the AddTreeViewController class.

--- TreeSapIOS/controller/AddTreeViewController.swift
+++ TreeSapIOS/controller/AddTreeViewController.swift
@@ -16,6 +16,7 @@ class AddTreeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    ///closes the modal (i.e. the page thing that appears in front of the previous view)
     @IBAction func closeAddTree(_ sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
         dismiss(animated: true, completion: nil)
