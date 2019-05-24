774bdd159a2fbc288b59cdbad61e7516c095fb3b
Josiah Brett <joisahqbrett@gmail.com>
2019-05-24 13:36:24 -0400

    The UserPreferences class was added, but contains little code. The text fields for the coordinate entry and the maximum identification distance were changed so that they now have clear buttons while they are being edited.

---
+++ TreeSapIOS/model/UserPreferences.swift
@@ -0,0 +1,21 @@
+//
+//  UserPreferences.swift
+//  TreeSapIOS
+//
+//  Created by CS Student on 5/24/19.
+//  Copyright © 2019 Hope CS. All rights reserved.
+//
+
+import Foundation
+
+class UserPreferences: NSCoder, NSCoding {
+    func encode(with aCoder: NSCoder) {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+    }
+    
+    
+}
