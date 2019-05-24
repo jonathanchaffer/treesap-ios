0018c04f5b6ec912dab0a8eaa59201bc9d8e7cfa
Josiah Brett <joisahqbrett@gmail.com>
2019-05-24 15:39:06 -0400

    A preliminary and potentially erroneous implementation of the UserPreferences class was implemented. This is an object that is used to encode and decode user preferences.

--- TreeSapIOS/model/UserPreferences.swift
+++ TreeSapIOS/model/UserPreferences.swift
@@ -8,13 +8,44 @@
 
 import Foundation
 
+//Based on code from https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/PersistData.html#//apple_ref/doc/uid/TP40015214-CH14-SW1
+///Used to encode and decode the users preferences
 class UserPreferences: NSCoder, NSCoding {
+    ///The maximum distance a tree can be from the location used in a search and still be found in the search
+    var maxIDDistance: Int
+    ///A dictionary that maps the names of data sources to booleans that indicate whether or not the data sources are active
+    var dataSources: [String: Bool]
+    ///Whether or not the user's location will be shown on the map
+    var showLocation: Bool
+    
+    ///This contains the strings that should be used as keys to encode and decode the instance variable of the UserPreferences class
+    struct preferenceKeys{
+        static let maxIDDistance = "masIDDistance"
+        static let dataSources = "dataSources"
+        static let showLocation = "showLocation"
+    }
+    
+    init(maxIDDistance: Int, dataSources: [String: Bool], showLocation: Bool) {
+        self.maxIDDistance = maxIDDistance
+        self.dataSources = dataSources
+        self.showLocation = showLocation
+        super.init()
+    }
+    
     func encode(with aCoder: NSCoder) {
-        
+        aCoder.encode(maxIDDistance, forKey: preferenceKeys.maxIDDistance)
+        aCoder.encode(dataSources, forKey: preferenceKeys.dataSources)
+        aCoder.encode(showLocation, forKey: preferenceKeys.showLocation)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        
+        maxIDDistance = aDecoder.decodeInteger(forKey: preferenceKeys.maxIDDistance)
+        guard let tempDataSources = aDecoder.decodeObject(forKey: preferenceKeys.dataSources) as? [String: Bool] else{
+            return nil
+        }
+        dataSources = tempDataSources
+        showLocation = aDecoder.decodeBool(forKey: preferenceKeys.showLocation)
+        super.init()
     }
     
     
