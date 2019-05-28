0aa47daffcca61a5d129e78a3c0c714a54231021
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-05-28 09:22:12 -0400

    made the dots static on the tree detail page view

--- TreeSapIOS/controller/TreeDetailPageViewController.swift
+++ TreeSapIOS/controller/TreeDetailPageViewController.swift
@@ -10,8 +10,12 @@
 import UIKit
 
 class TreeDetailPageViewController: UIPageViewController {
+    // MARK: Properties
+    /// The tree that will be displayed in the tree detail views.
     var displayedTree: Tree? = nil
-    
+    /// The dot indicator that shows the current page.
+    var pageControl: UIPageControl?
+    /// The pages to be displayed in the page view.
     fileprivate lazy var pages: [TreeDisplayViewController] = {
         return [
             self.getViewController(withIdentifier: "simpleDisplay"),
@@ -20,22 +24,18 @@ class TreeDetailPageViewController: UIPageViewController {
         ]
     }()
     
-    /**
-    Instantiates and returns a TreeDisplayViewController based on the identifier of the view controller in the storyboard. This function will probably cause the program to crash if no identifier matching the provided identifier is found.
-     - Parameter identifier: the storyboard ID of the view controller that is to be instantiated and returned
-     */
-    private func getViewController(withIdentifier identifier: String) -> TreeDisplayViewController {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier) as! TreeDisplayViewController
-    }
-    
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataSource = self
         self.delegate = self
         
-        //Makes the background white so it is not noticed when flipping quickly between the different tree displays
+        self.configurePageControl()
+        
+        // Set the background color to white so it is not noticed when flipping quickly between the different tree displays
         self.view.backgroundColor = UIColor.white
         
+        // Set the displayed tree for each of the tree detail views.
         for page in pages {
             page.displayedTree = self.displayedTree
         }
@@ -47,18 +47,43 @@ class TreeDetailPageViewController: UIPageViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        // Hide the tab bar when the detail display will appear.
         self.tabBarController?.tabBar.isHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        // Show the tab bar when the detail display will disappear.
         self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl!.currentPage = pages.index(of: pageContentViewController as! TreeDisplayViewController)!
+    }
+    
+    /**
+     Instantiates and returns a TreeDisplayViewController based on the identifier of the view controller in the storyboard.
+     - Parameter identifier: the storyboard ID of the view controller that is to be instantiated and returned.
+     */
+    private func getViewController(withIdentifier identifier: String) -> TreeDisplayViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier) as! TreeDisplayViewController
+    }
+    
+    // MARK: Private methods
+    /// Sets up the dot indicator that shows the current page.
+    private func configurePageControl() {
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: UIScreen.main.bounds.maxY - 50, width: UIScreen.main.bounds.width, height: 50))
+        pageControl!.numberOfPages = pages.count
+        pageControl!.currentPage = 0
+        pageControl!.currentPageIndicatorTintColor = UIColor(red: 0.373, green: 0.718, blue: 0.306, alpha: 1.0)
+        pageControl!.pageIndicatorTintColor = UIColor(red: 0.373, green: 0.718, blue: 0.306, alpha: 0.3)
+        self.view.addSubview(pageControl!)
     }
 }
 
 
 
 extension TreeDetailPageViewController: UIPageViewControllerDataSource {
-    
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let viewControllerIndex = pages.firstIndex(of: viewController as! TreeDisplayViewController) else { return nil }
         let previousIndex = viewControllerIndex - 1
