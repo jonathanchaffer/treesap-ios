e90131d54bdd254bdaec24dd4dc859c495fb7795
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-05-28 15:27:26 -0400

    added pie chart display (doesn't show actual data yet)

--- TreeSapIOS/controller/PieChartDisplayViewController.swift
+++ TreeSapIOS/controller/PieChartDisplayViewController.swift
@@ -7,13 +7,50 @@
 //
 
 import UIKit
+import Charts
 
-class PieChartDisplayViewController: TreeDisplayViewController {
-    
+class PieChartDisplayViewController: TreeDisplayViewController, ChartViewDelegate {
+    // MARK: - Properties
+    @IBOutlet weak var chartView: PieChartView!
+
+    // MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        chartView.delegate = self
+        chartView.drawHoleEnabled = false
+        let legend = chartView.legend
+        legend.verticalAlignment = .top
+        legend.orientation = .vertical
+        legend.font = .systemFont(ofSize: 17, weight: .regular)
+        self.updateChartData()
+        chartView.animate(yAxisDuration: 1, easingOption: .easeOutBounce)
     }
-
+    
+    // MARK: - Private methods
+    private func updateChartData() {
+        let entries = (0..<4).map { (i) -> PieChartDataEntry in
+            return PieChartDataEntry(value: Double.random(in: 0...100), label: "test")
+        }
+        
+        let set = PieChartDataSet(entries: entries, label: "")
+        set.drawIconsEnabled = false
+        set.sliceSpace = 2
+        
+        set.colors = []
+        for i in 0..<4 {
+            let redOffset = CGFloat(Double(i) * -0.2)
+            let greenOffset = CGFloat(Double(i) * -0.15)
+            let blueOffset = CGFloat(Double(i) * -0.2)
+            set.colors.append(UIColor(red: 0.373 + redOffset, green: 0.718 + greenOffset, blue: 0.306 + blueOffset, alpha: 1.0))
+        }
+        
+        let data = PieChartData(dataSet: set)
+        
+        data.setValueFont(.systemFont(ofSize: 17, weight: .regular))
+        data.setValueTextColor(.white)
+        
+        chartView.data = data
+        chartView.highlightValues(nil)
+    }
 }
--- TreeSapIOS/model/TreePointAnnotationView.swift
+++ TreeSapIOS/model/TreePointAnnotationView.swift
@@ -17,7 +17,7 @@ class TreePointAnnotationView: MKAnnotationView {
             let redOffset = CGFloat(Float.random(in: -0.1...0.1))
             let greenOffset = CGFloat(Float.random(in: -0.1...0.1))
             let blueOffset = CGFloat(Float.random(in: -0.1...0.1))
-            tintColor = UIColor(red: 0.4470588235 + redOffset, green: 0.7411764706 + greenOffset, blue: 0.3529411765 + blueOffset, alpha: 1.0)
+            tintColor = UIColor(red: 0.447 + redOffset, green: 0.741 + greenOffset, blue: 0.353 + blueOffset, alpha: 1.0)
         }
     }
     
