87d5f766f41ba7e3d5321a0bbf3441f98b9bbfe9
Josiah Brett <joisahqbrett@gmail.com>
2019-05-29 11:31:54 -0400

    Added the UserPreferenceKeys struct, which stores the keys that are to be used to store the user preferences and default preferences.

---
+++ TreeSapIOS/model/UserPreferenceKeys.swift
@@ -0,0 +1,17 @@
+//
+//  UserPreferenceKeys.swift
+//  TreeSapIOS
+//
+//  Created by CS Student on 5/29/19.
+//  Copyright © 2019 Hope CS. All rights reserved.
+//
+
+///Used to store the keys that are used to store the user preferences and default preferences
+struct UserPreferenceKeys{
+    let cutoffDistance = "cutoffDistance"
+    let cutoffDistanceDefault = "cutoffDistanceDefault"
+    let dataSources = "dataSources"
+    let dataSourcesDefault = "dataSourcesDefault"
+    let showUserLocation = "showUserLocation"
+    let showUserLocationDefault = "showUserLocationDefault"
+}
