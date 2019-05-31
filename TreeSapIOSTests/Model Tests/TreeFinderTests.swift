5caaf839fe916f982ed80f448b15bd996be69be3
Josiah Brett <joisahqbrett@gmail.com>
2019-05-31 11:52:26 -0400

    Added TreeFinderTests class and added tests for the distanceBetween function in the TreeFinder class.

--- TreeSapIOSTests/Model Tests/TreeFinderTests.swift
+++ TreeSapIOSTests/Model Tests/TreeFinderTests.swift
@@ -8,9 +8,26 @@
 
 import XCTest
 @testable import TreeSapIOS
+import MapKit
 
 class TreeFinderTests: XCTestCase {
-
+    let distanceBetweenMarginOfError: Double = 0.00001
+    
+    /**
+     Uses the distance formula to calculate the distance between to points
+     
+     - Parameters:
+     - point1: A CLLocationCoordinate2D that gives the location of the first point
+     - point2: A CLLocationCoordinate2D that gives the location of the second point
+     
+     - Returns: A double that gives the distance between the two points
+     */
+    func distanceBetweenPoints(point1: CLLocationCoordinate2D, point2: CLLocationCoordinate2D) -> Double{
+        let latitudeDistance: Double = point1.latitude - point2.latitude
+        let longitudeDistance: Double = point1.longitude - point2.longitude
+        return ((latitudeDistance * latitudeDistance) + (longitudeDistance * longitudeDistance)).squareRoot()
+    }
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,15 +36,32 @@ class TreeFinderTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDistanceBetweenWithCases(){
+        //latitude value sare in the range [-90, 90] and longitude values are in the range [-180, 180]
+        let distanceBetweenTestCases = [(38.1358762, 68.4387746, 38.1358762, 68.4387746), (-90, -270, 90, 270)]
+        
+        for testCase in distanceBetweenTestCases{
+            let point1 = CLLocationCoordinate2D(latitude: testCase.0, longitude: testCase.1)
+            let point2 = CLLocationCoordinate2D(latitude: testCase.2, longitude: testCase.3)
+            
+            let testDistance: Double = distanceBetweenPoints(point1: point1, point2: point2)
+            let actualDistance: Double = TreeFinder.distanceBetween(from: point1, to: point2)
+            
+            XCTAssertEqual(actualDistance, testDistance, accuracy: distanceBetweenMarginOfError, "Expected the calculated distance between points to be close to the expected value.")
+        }
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testDistanceBetweenWithRandomGenerator(){
+        let numCases = 1000
+        
+        for _ in 0 ..< numCases{
+            let point1 = CLLocationCoordinate2D(latitude: Double.random(in: -90 ... 90), longitude: Double.random(in: -180 ... 180))
+            let point2 = CLLocationCoordinate2D(latitude: Double.random(in: -90 ... 90), longitude: Double.random(in: -180 ... 180))
+            
+            let testDistance: Double = distanceBetweenPoints(point1: point1, point2: point2)
+            let actualDistance: Double = TreeFinder.distanceBetween(from: point1, to: point2)
+            
+            XCTAssertEqual(actualDistance, testDistance, accuracy: distanceBetweenMarginOfError, "Expected the calculated distance between points to be close to the expected value.")
         }
     }
 
