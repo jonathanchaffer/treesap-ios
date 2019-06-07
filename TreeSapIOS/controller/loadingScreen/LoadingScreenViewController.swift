c7e65b952d5d67ee000d15fc55a205e2dd2a4209
Josiah Brett <josiah.brett@hope.edu>
2019-06-07 13:51:34 -0400

    Some of the LoadingScreenViewController class was modified.

--- TreeSapIOS/controller/loadingScreen/LoadingScreenViewController.swift
+++ TreeSapIOS/controller/loadingScreen/LoadingScreenViewController.swift
@@ -18,7 +18,7 @@ class LoadingScreenViewController: UIViewController{
         // Load trees, assumming there is internet access. If tree data could be loaded adequately from local repositories, exit the loading screen before loading tree data from online repositories. Otherwise, stay in the loading screen until the tree data from online repositories is loaded.
         let allDataSourcesLoaded = appDelegate.importLocalTreeData()
         if(allDataSourcesLoaded){
-            loadHomeScreen(localDataLoaded: true)
+            loadHomeScreen()
             appDelegate.importOnlineTreeData()
         }else{
             appDelegate.importOnlineTreeData()
@@ -31,17 +31,11 @@ class LoadingScreenViewController: UIViewController{
      Loads the home screen. If the parameter indicates that the local data could not be loaded, alerts the user upon loading the home screen.  Made using code from https://coderwall.com/p/cjuzng/swift-instantiate-a-view-controller-using-its-storyboard-name-in-xcode as a reference.
      - Parameter localDataLoaded: Whether all of the local data was loaded properly. If false, the user is alerted.
      */
-    func loadHomeScreen(localDataLoaded: Bool){
+    func loadHomeScreen(){
         //Load home screen
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let tabBarController: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "tabBarController")
+        tabBarController.modalTransitionStyle = .crossDissolve
         self.show(tabBarController, sender: self)
-        
-        //If not all of the local data was loaded alert the user
-        if(!localDataLoaded){
-            let alertController = UIAlertController(title: "", message: "Some of the tree information could not be loaded", preferredStyle: UIAlertController.Style.alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-            tabBarController.present(alertController, animated: true, completion: nil)
-        }
     }
 }
