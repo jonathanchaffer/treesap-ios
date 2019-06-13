1a673b0dafa4c53c6ea334d3c1511de1d9f7054d
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-06-13 16:11:12 -0400

    cleaned up TreeFinder documentation and removed unused method

--- TreeSapIOS/model/TreeFinder.swift
+++ TreeSapIOS/model/TreeFinder.swift
@@ -11,11 +11,12 @@ import MapKit
 
 class TreeFinder {
     /**
-     Searches for the Tree object closest to the specified location that is within the specified distance of the location. The search is conducted using the specified data sources. If no trees are found within the cutoff distance of the specified location, the function returns nil.
+     Searches for the Tree object closest to the specified location within the specified cutoff distance from the location.
      - Parameters:
-     - location: The location that is used in the search
-     - dataSources: An array of DataSource objects that contain the Tree objects that are searched through
-     - cutoffDistance: The cutoff distance for the search
+        - location: The location used in the search.
+        - dataSources: An array of DataSources to be searched.
+        - cutoffDistance: The cutoff distance for the search.
+     - Returns: The found Tree, or nil if none was found.
      */
     class func findTreeByLocation(location: CLLocationCoordinate2D, dataSources: [DataSource], cutoffDistance: Double) -> Tree? {
         var closestTree: Tree?
@@ -35,12 +36,13 @@ class TreeFinder {
     }
 
     /**
-     Searches for the Tree object closest to the specified location that is within the specified distance of the location. The search is conducted using the specified data sources. If no trees are found within the cutoff distance of the specified location, the function returns nil.
+     Searches for the Tree object closest to the specified location within the specified cutoff distance from the location.
      - Parameters:
-     - latitude: the latitude of the location used in the search
-     - longitude: the longitude of the location used in the search
-     - dataSources: An array of DataSource objects that contain the Tree objects that are searched through
-     - cutoffDistance: the cutoff distance for the search
+         - latitude: The latitude of the location used in the search.
+         - longitude: The longitude of the location used in the search.
+         - dataSources: An array of DataSources to be searched.
+         - cutoffDistance: The cutoff distance for the search.
+     - Returns: The found Tree, or nil if none was found.
      */
     class func findTreeByLocation(latitude: Double, longitude: Double, dataSources: [DataSource], cutoffDistance: Double) -> Tree? {
         let locationCoordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
@@ -51,36 +53,5 @@ class TreeFinder {
         let locationFrom = CLLocation(latitude: from.latitude, longitude: from.longitude)
         let locationTo = CLLocation(latitude: to.latitude, longitude: to.longitude)
         return locationFrom.distance(from: locationTo)
-    }
-
-    /**
-     Searches for a Tree object in the DataSource with the given name (.dataSourceName) using the ID of the Tree (.id)
-
-     - Parameters:
-     - treeId: the ID of the tree that is to be searched for
-     - dataSourceName: the name of the DataSource object that contains the Tree object, if it exists
-     - dataSources: an array of Datasources which includes the given DataSource object, if it exists
-     */
-    class func findTreeByID(treeID: Int, dataSourceName: String, dataSources: [DataSource]) -> Tree? {
-        // Find the DataSource in which the tree is to be found based on the name
-        var dataSource: DataSource!
-        var isFound: Bool = false
-        for source in dataSources {
-            if source.dataSourceName == dataSourceName {
-                dataSource = source
-                isFound = true
-            }
-        }
-        if !isFound {
-            return nil
-        }
-
-        // Find the tree in the data source based on the ID of the tree
-        for tree in dataSource.trees {
-            if tree.id == treeID {
-                return tree
-            }
-        }
-        return nil
     }
 }
