9a57de463f588f3df5eb707eda7f646e6b8bf3f6
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-06-18 08:55:29 -0400

    fixed bug where all trees show up blue when not logged in

--- TreeSapIOS/model/TreeAnnotation.swift
+++ TreeSapIOS/model/TreeAnnotation.swift
@@ -19,7 +19,7 @@ class TreeAnnotation: NSObject, MKAnnotation {
         let redOffset = CGFloat(Float.random(in: -0.1 ... 0.1))
         let greenOffset = CGFloat(Float.random(in: -0.1 ... 0.1))
         let blueOffset = CGFloat(Float.random(in: -0.1 ... 0.1))
-        if tree.userID == AccountManager.getUserID() {
+        if tree.userID == AccountManager.getUserID() && AccountManager.getUserID() != nil {
             return UIColor(red: 46/255 + redOffset, green: 129/255 + greenOffset, blue: 219/255 + blueOffset, alpha: 1.0)
         } else {
             return UIColor(red: 0.447 + redOffset, green: 0.741 + greenOffset, blue: 0.353 + blueOffset, alpha: 1.0)
