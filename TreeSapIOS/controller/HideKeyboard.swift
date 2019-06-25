fb5fec662f8d3141f3f98dee4487f113e00fc745
Josiah Brett <josiah.brett@hope.edu>
2019-06-25 17:10:01 -0400

    Changed the HideKeyboard file so that if the user taps on a button or text field, the keyboard will not be dismissed.

--- TreeSapIOS/controller/HideKeyboard.swift
+++ TreeSapIOS/controller/HideKeyboard.swift
@@ -10,15 +10,28 @@ import Foundation
 import UIKit
 
 /// Extension that hides the keyboard when it is tapped outside. Simply add 'self.hideKeyboardWhenTappedAround()' to viewDidLoad to bring this behavior into effect.
-extension UIViewController {
+extension UIViewController: UIGestureRecognizerDelegate {
     func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard(sender:)))
+        tap.delegate = self
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
     
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool{
+        let touchView = touch.view
+        if(touchView == nil){
+            return true
+        }
+        
+        if(touchView is UIButton || touchView is UITextField){
+            return false
+        }
+        return true
+    }
+    
     ///Dismisses the keyboard.
-    @objc func dismissKeyboard() {
+    @objc func dismissKeyboard(sender: UITapGestureRecognizer) {
         //The dispatch queue is used because button presses need to be resolved before the keyboard is dismissed.
         DispatchQueue.main.asyncAfter(deadline: .now()){
             self.view.endEditing(true)
