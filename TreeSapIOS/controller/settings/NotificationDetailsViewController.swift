e775173e3accb24ad806ee38433eeccec9f28b8c
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-06-27 15:56:14 -0400

    added latitude and longitude to notification details

--- TreeSapIOS/controller/settings/NotificationDetailsViewController.swift
+++ TreeSapIOS/controller/settings/NotificationDetailsViewController.swift
@@ -15,6 +15,8 @@ class NotificationDetailsViewController: UIViewController {
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var commonNameLabel: UILabel!
     @IBOutlet weak var scientificNameLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var dbhLabel: UILabel!
     @IBOutlet weak var commentsLabel: UILabel!
     
@@ -33,6 +35,8 @@ class NotificationDetailsViewController: UIViewController {
         let treeData = data!["treeData"]! as! [String: Any]
         let commonName = treeData["commonName"]! as! String
         let scientificName = treeData["scientificName"]! as! String
+        let latitude = treeData["latitude"]! as! Double
+        let longitude = treeData["longitude"] as! Double
         let dbhArray = treeData["dbhArray"]! as! [Double]
 
         // Set title and subtitle
@@ -64,6 +68,10 @@ class NotificationDetailsViewController: UIViewController {
         } else {
             scientificNameLabel.text = "N/A"
         }
+        
+        // Set latitude and longitude labels
+        latitudeLabel.text = String(latitude)
+        longitudeLabel.text = String(longitude)
         
         // Set DBH label
         if dbhArray != [] {
