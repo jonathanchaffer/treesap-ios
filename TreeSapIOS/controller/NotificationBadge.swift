ee9e4f94ba9e6d1f0c6117c7bdd295eb6b0243f5
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-07-08 10:59:19 -0400

    updated notification badge

--- TreeSapIOS/controller/NotificationBadge.swift
+++ TreeSapIOS/controller/NotificationBadge.swift
@@ -5,6 +5,8 @@
 //  Created by Jonathan Chaffer in Summer 2019.
 //  Copyright © 2019 Hope CS. All rights reserved.
 //
+//  Used http://www.stefanovettor.com/2016/04/30/adding-badge-uibarbuttonitem/ as a reference.
+//
 
 import UIKit
 
@@ -19,22 +21,24 @@ extension CAShapeLayer {
 
 extension UIViewController {
     func updateNotificationBadge() {
-        NotificationCenter.default.addObserver(self, selector: #selector(showBadge), name: NSNotification.Name(StringConstants.unreadNotificationsCountNotification), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(addBadge), name: NSNotification.Name(StringConstants.unreadNotificationsCountNotification), object: nil)
         DatabaseManager.retrieveNumberOfUnreadNotifications()
     }
     
-    @objc private func showBadge(_ notification: Notification) {
-        let numNotifications = notification.userInfo!["count"] as! Int
+    @objc private func addBadge(_ notification: Notification) {
+        // Initialize the badge
         let button = self.navigationController?.navigationBar.items![0].leftBarButtonItem!
         guard let view = button!.value(forKey: "view") as? UIView else { return }
-        if numNotifications > 0 {
-            // Initialize the badge
-            let badge = CAShapeLayer()
-            let radius = CGFloat(4)
-            let offset = CGPoint(x: 32, y: 10)
-            let location = CGPoint(x: (radius + offset.x), y: (radius + offset.y))
-            let color = UIColor(named: "notificationBadge")!
-            badge.drawCircleAtLocation(location: location, withRadius: radius, andColor: color)
+        let badge = CAShapeLayer()
+        let radius = CGFloat(4)
+        let offset = CGPoint(x: 32, y: 10)
+        let location = CGPoint(x: (radius + offset.x), y: (radius + offset.y))
+        let color = UIColor(named: "notificationBadge")!
+        badge.drawCircleAtLocation(location: location, withRadius: radius, andColor: color)
+        // Add the badge if there are new notifications
+        let numNotifications = notification.userInfo!["count"] as! Int
+        print(numNotifications)
+        if numNotifications != 0 {
             view.layer.addSublayer(badge)
         }
     }
