e56c3d35c7a30e58b95edf066caf4185a451e24e
jonathanchaffer <jonathan.chaffer@hope.edu>
2019-07-09 09:46:30 -0400

    fixed notification badge flashing when switching between tabs

---
+++ TreeSapIOS/model/stateManagers/UnreadNotificationsManager.swift
@@ -0,0 +1,33 @@
+//
+//  UnreadManager.swift
+//  TreeSapIOS
+//
+//  Created by Jonathan Chaffer in Summer 2019.
+//  Copyright © 2019 Hope CS. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class UnreadManager {
+    /// The Firestore database.
+    static var db = Firestore.firestore()
+    /// The number of unread notifications.
+    static var numUnreadNotifications = 0
+    
+    static func retrieveNumberOfUnreadNotifications() {
+        if AccountManager.getUserID() != nil {
+            let query = db.collection("notifications").whereField(FieldPath(["treeData", "userID"]), isEqualTo: AccountManager.getUserID()!).whereField("read", isEqualTo: false)
+            query.getDocuments() { snapshot, error in
+                if let error = error {
+                    print(error)
+                } else {
+                    numUnreadNotifications = snapshot!.count
+                    NotificationCenter.default.post(name: NSNotification.Name(StringConstants.unreadNotificationsCountNotification), object: nil)
+                }
+            }
+        } else {
+            return
+        }
+    }
+}
